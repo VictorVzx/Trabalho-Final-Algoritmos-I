0c3092a6f2c83d432ccfdd31e604c4c418d8d701
victorvzx <gifpa45we@gmail.com>
2026-05-31 12:21:43 -0300

    feat(comments): adicionar comentarios ao codigo principal

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
+// standard in out, printf, scanf e as demais funções essenciais
 #include <stdio.h>
+
+//prototipo de funções (interfaces)
 #include "interfaces/menuPrincipal/interfaceMenuPrincipal.h"
+
+//prototipo de structs
 #include "structs/structs.h"
 
+// limite de abelhas e sensores 
 #define MAX_ABELHAS 50
 #define MAX_SENSORES 100
 
+
+// função main (implementação do codigo principal)
 int main(void){
+
+    //variaveis de opção, para checar o do while
     int menuOption, abelhasOption, sensoresOption;
     do{
+        //chamando a função de mostrar o menu
         interfaceMenuPrincipal();
         scanf("%d", &menuOption);
     }while(menuOption != 4);
