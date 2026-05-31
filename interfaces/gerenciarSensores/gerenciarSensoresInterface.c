0870bb8c61c2fb16133983d12979d34fa35659de
victorvzx <gifpa45we@gmail.com>
2026-05-31 11:43:04 -0300

    feat(interface): finalizar interface de gerenciamento de sensores

---
+++ interfaces/gerenciarSensores/gerenciarSensoresInterface.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "gerenciarSensoresInterface.h"
+
+void gerenciarSensoresInterface(){
+    printf("6. Cadastrar sensor\n");
+    printf("7. Listar sensores\n");
+    printf("8. Buscar por ID da abelha\n");
+    printf("9. Alterar leitura\n");
+    printf("10. Remover\n");
+    printf("11. Voltar\n");
+    printf("-> ");
+}
---
+++ interfaces/gerenciarSensores/gerenciarSensoresInterface.h
@@ -0,0 +1,6 @@
+#ifndef GERENCIARSENSORESINTERFACE_H
+#define GERENCIARSENSORESINTERFACE_H
+
+void gerenciarSensoresInterface();
+
+#endif
