acc3b729ee5286d2cfd4babfd343820347c26b08
victorvzx <gifpa45we@gmail.com>
2026-05-31 11:34:11 -0300

    feat(interface): finalizar interface de gerenciamento de abelhas

--- interfaces/gerenciarAbelhas/gerenciarAbelhasInterface.c
+++ interfaces/gerenciarAbelhas/gerenciarAbelhasInterface.c
@@ -2,5 +2,11 @@
 #include "gerencialAbelhasInterface.h"
 
 void showGerenciarAbelhas(){
-    
+    printf("6. Cadastrar abelha\n");
+    printf("7. Listar todas\n");
+    printf("8. Buscar por nome popular\n");
+    printf("9. Alterar dados\n");
+    printf("10. Remover\n");
+    printf("11. Voltar\n");
+    printf("-> ");
 }
