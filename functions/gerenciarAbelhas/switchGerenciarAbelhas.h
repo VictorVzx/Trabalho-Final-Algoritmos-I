25a0c0f2a3f2360849eb48094c6925bda5493ed2
victorvzx <gifpa45we@gmail.com>
2026-06-05 18:59:19 -0300

    fix(abelhas): adicionar structs ao prototipo de função do switch de gerenciar abelhas

--- functions/gerenciarAbelhas/switchGerenciarAbelhas.h
+++ functions/gerenciarAbelhas/switchGerenciarAbelhas.h
@@ -1,6 +1,8 @@
 #ifndef SWITCHGERENCIARABELHAS_H
 #define SWITCHGERENCIARABELHAS_H
 
+#include "../../structs/structs.h"
+
 
 // prototipo do switch para gerenciar abelhas
 void switchGerenciarAbelhas(Abelha a[]);
