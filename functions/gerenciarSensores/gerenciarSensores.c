dbbc1ade632e62e03bd50ba2b47c35c6048d9485
victorvzx <gifpa45we@gmail.com>
2026-06-05 21:22:20 -0300

    fix(lixo): remover lixo restante no codigo de gerenciar sensores

--- functions/gerenciarSensores/gerenciarSensores.c
+++ functions/gerenciarSensores/gerenciarSensores.c
@@ -148,52 +148,6 @@ void cadastrarSensor(Sensor s[], Abelha a[], int qtdAbelhas){
 
 }
 
-// void cadastrarSensor(Sensor s[])
-// {
-//     // mostra interface de cadastrar abelhas
-//     showCadastrarSensores();
-
-//     char tecla;
-//     novosSensores = 0;
-
-//     // pergunta a quantidade de abelhas antes de adicionar
-//     printf("Quantos sensores voce quer cadastrar?: ");
-//     scanf("%d", &novosSensores);
-//     limparBuffer();
-
-//     if(qtd > 100){
-//         printf("Não pode ter mais do que 100 sensores.\n\n");
-//         do
-//         {
-//             printf("\nPressione ENTER para sair...");
-//             while (getchar() != '\n');
-//             tecla = '\n';
-//         } while (tecla != '\n');
-//         novosSensores = 0;
-//         return;
-//     }
-
-//     // id é inicializado com a quantidade atual de sensores
-//     int id = qtd;
-
-//     // loop vai da quantidade atual de sensores até a nova quantidade que o usuario deseja registrar
-//     for (int i = qtd; i < qtd + novosSensores; i++)
-//     {
-//         limparTela();
-//         showGerenciarSensores();
-//         sleep(1);
-//         printf("======== %dº SENSOR ========\n", i + 1);
-
-
-//         id++;
-
-//         s[i].id = id;
-
-//     }
-//     // quantidade é somado com a quantidade de novas abelhas
-//     qtd += novosSensores;
-// }
-
 // void listarSensores(Sensor s[]);
 
 // void buscarSensorPorIdAbelha(Sensor s[]);
