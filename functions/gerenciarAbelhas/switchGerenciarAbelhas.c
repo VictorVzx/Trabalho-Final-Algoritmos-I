d3bcf8279897ed9882fe01acc1a768ad6375a760
victorvzx <gifpa45we@gmail.com>
2026-06-06 23:28:59 -0300

    refactor(abelhas): melhorar visual do menu de abelhas

--- functions/gerenciarAbelhas/switchGerenciarAbelhas.c
+++ functions/gerenciarAbelhas/switchGerenciarAbelhas.c
@@ -9,7 +9,8 @@ void switchGerenciarAbelhas(Abelha a[])
 {
     int abelhasOption;
     do
-    {  
+    {   
+        limparTela();
         showGerenciarAbelhas();
         scanf("%d", &abelhasOption);
         limparBuffer();
@@ -18,16 +19,13 @@ void switchGerenciarAbelhas(Abelha a[])
         {
         case 6:
             limparTela();
-            // chama a função de cadastrar abelhas passando como parametro o sistema para chamar a função "limparTela()", que recebe como parametro o numero do sistema operacional
             cadastrarAbelha(a);
-            printf("\nAguarde...\n");
-            limparTela();
             break;
         case 7:
+            printf("\nAguarde...\n");
+            sleep(1);
             limparTela();
             listarTodas(a);
-            printf("\nAguarde...\n");
-            limparTela();
             break;
         case 8:
             limparTela();
@@ -40,12 +38,10 @@ void switchGerenciarAbelhas(Abelha a[])
         case 10:
             limparTela();
             removerAbelha(a);
-            limparTela();
             break;
         case 11:
             limparTela();
             printf("\nVoltando...\n");
-            limparTela();
             break;
         default:
             limparTela();
