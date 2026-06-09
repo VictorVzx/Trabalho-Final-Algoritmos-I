1ed08d1c4ba1881c57ac1529f189d2903e8ad330
victorvzx <gifpa45we@gmail.com>
2026-06-09 19:02:00 -0300

    fix(sensores): resolver cores do confirmar editar sensores
    - As cores do confirmar editar sensores pegavam todo o resto do codigo de confirmar
    - Adicionei um RESET depois da opção 'sim'

--- functions/gerenciarSensores/gerenciarSensores.c
+++ functions/gerenciarSensores/gerenciarSensores.c
@@ -234,7 +234,7 @@ void alterarLeitura(Sensor s[], Abelha a[], int qtdAbelhas){
     int confirmarEdicao = 0;
     do{
         limparTela();
-        printf(YELLOW BOLD "Tem certeza que deseja editar este sensor?\n"RESET YELLOW BOLD "\n1 - Sim\n" RESET GREEN BOLD "2 - Não\n\n-> " RESET);
+        printf(YELLOW BOLD "Tem certeza que deseja editar este sensor?\n"RESET YELLOW BOLD "\n1 - Sim\n" RESET GREEN BOLD "2 - Não\n" RESET "\n-> " RESET);
         scanf("%d", &confirmarEdicao);
         limparBuffer();
         if(confirmarEdicao == 2){
