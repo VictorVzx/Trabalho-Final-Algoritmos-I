edaecac1e9be1cf1a74668a5a61f0931229c8d06
victorvzx <gifpa45we@gmail.com>
2026-06-11 17:39:14 -0300

    feat(docs): adicionar comentarios às abelhas

--- functions/gerenciarAbelhas/switchGerenciarAbelhas.c
+++ functions/gerenciarAbelhas/switchGerenciarAbelhas.c
@@ -1,56 +1,80 @@
+// BIBLIOTECAS PADRÃO, STDIO E UNISTD
 #include <stdio.h>
+
+// UNISTD PARA UTILIZAR A FUNÇÃO SLEEP
 #include <unistd.h>
 
+// INCLUI CABEÇALHO DO ARQUIVO
 #include "gerenciarAbelhas.h"
+
+// INCLUI CABEÇALHOS DE INTERFACE (VISUAL)
 #include "../../interfaces/gerenciarAbelhas/gerenciarAbelhasInterface.h"
+
+// INCLUI FUNÇÕES DE UTILIDADE, LIMPAR TELA E LIMPAR BUFFER
 #include "../limparTela/limparTela.h"
 #include "../limparBuffer/limparBuffer.h"
+
+// INCLUI STRUCTS PARA PASSAR COMO PARAMETRO
 #include "../../structs/structs.h"
 
+// IMPLEMENTAÇÃO DA FUNÇÃO DE NAVEGAÇÃO DO MENU GERENCIAR ABELHAS
 void switchGerenciarAbelhas(Abelha a[])
-{
-    int abelhasOption;
+{   
+    // RECEBE VARIAVEL OPÇÃO
+    int abelhasOption;  
     do
     {   
         limparTela();
         showGerenciarAbelhas();
+
+        // LÊ A VARIAVEL OPÇÃO
         scanf("%d", &abelhasOption);
         limparBuffer();
 
         limparTela();
         
+        // ESTRUTURA DE SELEÇÃO DE OPÇÕES COM SWITCH CASE
         switch (abelhasOption)
         {
+        // PRIMEIRA OPÇÃO: 6, CADASTRAR ABELHAS
         case 6:
             limparTela();
             cadastrarAbelha(a);
             break;
+        // SEGUNDA OPÇÃO: 7, LISTAR TODAS
         case 7:
             printf("\nAguarde...\n");
             sleep(1);
             limparTela();
             listarTodas(a);
             break;
+        // TERCEIRA OPÇÃO: 8, BUSCAR POR NOME POPULAR
         case 8:
             limparTela();
             buscarPorNomePopular(a);
             break;
+        // QUARTA OPÇÃO, 9: LIMPAR TELA
         case 9:
             limparTela();
             editarAbelha(a);
             break;
+        // QUINTA OPÇÃO: 10, REMOVER ABELHA
         case 10:
             limparTela();
             removerAbelha(a);
             break;
+        // SEXTA OPÇÃO: 11, SAIR DO MENU
         case 11:
             limparTela();
             printf("\nVoltando...\n");
             break;
+        // PADRÃO: OPÇÃO INVALIDA
         default:
             limparTela();
             printf("\nOpção invalida\n");
+            sleep(1);
             break;
         }
+    // CONDIÇÃO FINAL: ENCERRA O LOOP SE A OPÇÃO SELECIONADA FOR 11
     }while (abelhasOption != 11);
 }
